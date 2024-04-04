341b10ac4e3b3349a43808aeeb688614b6b4147a
Jay Dwayne <45174746+JayDwayne@users.noreply.github.com>
2024-04-04 12:01:21 +0300

    Kudos! Turnkey Africa

---
+++ Turnkey-Solutions-Task/src/TurnkeyAfrica.java
@@ -0,0 +1,7 @@
+public class TurnkeyAfrica {
+    public static void main(String[] args) {
+        System.out.println("Turnkey Africa is doing such a fantastic work!");
+        System.out.println("Their innovative solutions are transforming the digital landscape.");
+        System.out.println("Keep up the amazing work, Turnkey Africa!");
+    }
+}
